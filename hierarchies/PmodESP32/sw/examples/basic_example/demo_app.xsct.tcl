5554e880a6e2e3422ca1d8a8e4fcd2e69bd2e848
ArtVVB <arthur.brown@digilent.com>
2021-12-30 16:00:21 -0800

    Add most Pmod Hierarchy sw
    The Pmod MTDS, WIFI, and SD are excluded
    
    Hierarchies testing: Refactor PmodToF sw
    Hierarchies testing: Add PmodTMP3 sw
    Hierarchies testing: Add PmodTC1 sw
    Hierarchies testing: Add PmodSF3 sw
    Hierarchies testing: Add PmodOLEDrgb sw
    Hierarchies testing: Add PmodRTCC sw
    Hierarchies testing: Add PmodR2R sw
    Hierarchies testing: Add PmodOLED sw
    Hierarchies testing: Add PmodPIR sw
    Hierarchies testing: Refactor PmodGPS sw
        Remove UART drivers that are brought in via the bsp
    Hierarchies testing: Add PmodMAXSONAR sw
    Hierarchies testing: Add PmodKYPD sw
    Hierarchies testing: Refactor PmodHYGRO sw
    Hierarchies testing: Add PmodGYRO sw
    Hierarchies testing: Add PmodESP32 sw
    Hierarchies testing: Add PmodENC sw
    Hierarchies testing: Add PmodDPG1 sw
    Hierarchies testing: Add PmodDHB1 sw
    Hierarchies testing: Refactor PmodDA4 sw
        Remove xspi drivers that are brought in through the bsp
    Hierarchies testing: Add PmodDA1 sw
    Hierarchies testing: Add PmodCOLOR sw
    Hierarchies testing: Add PmodCMPS2 sw
    Hierarchies testing: Add PmodCLS sw
    Hierarchies testing: Add PmodCAN sw
    Hierarchies testing: Refactor PmodBT2 sw
    Hierarchies testing: Add PmodBLE sw
    Hierarchies testing: Add PmodAQS sw
    Hierarchies testing: Add PmodAMP2 sw
    Hierarchies testing: Add PmodALS sw
    Hierarchies testing: Add PmodAD5 sw
    Hierarchies testing: Rename PmodAD1 sw folder
    Hierarchies testing: Add PmodAD2 sw
    Hierarchies testing: Add PmodACL2 sw
    Hierarchies testing: Add PmodACL sw

---
+++ hierarchies/PmodESP32/sw/examples/basic_example/demo_app.xsct.tcl
@@ -0,0 +1,25 @@
+puts "INFO: Running [info script]"
+
+namespace eval PmodESP32_basic_example {
+    variable script_dir
+
+    proc init {} {
+        # script_dir variable is used here because when a process is called that contains the info script command,
+        # info script returns the path to the script calling the proc rather than to this script
+        set script [info script] 
+        set PmodESP32_basic_example::script_dir [file normalize [file dirname $script]]
+    }
+
+    proc create_app {app_name sysproj_name domain_name platform_name} {
+        set lang "c"
+        set template "Empty Application(C)"
+        app create -name ${app_name} -lang ${lang} -template ${template} -domain ${domain_name} -platform ${platform_name} -sysproj ${sysproj_name}
+        set app_sources [file join ${PmodESP32_basic_example::script_dir} sources]
+        set drivers [file join [file dirname [file dirname ${PmodESP32_basic_example::script_dir}]] sources]
+        importsources -name ${app_name} -path ${app_sources}
+        importsources -name ${app_name} -path ${drivers}
+        # note: configure the linker script?
+    }
+}
+
+PmodESP32_basic_example::init
---
+++ hierarchies/PmodNAV/sw/examples/basic_example/demo_app.xsct.tcl
@@ -0,0 +1,28 @@
+puts "INFO: Running [info script]"
+
+namespace eval PmodNAV_basic_example {
+    variable script_dir
+
+    proc init {} {
+        # script_dir variable is used here because when a process is called that contains the info script command,
+        # info script returns the path to the script calling the proc rather than to this script
+        set script [info script] 
+        set PmodNAV_basic_example::script_dir [file normalize [file dirname $script]]
+    }
+
+    proc create_app {app_name sysproj_name domain_name platform_name} {
+        set lang "c"
+        set template "Empty Application(C)"
+        app create -name ${app_name} -lang ${lang} -template ${template} -domain ${domain_name} -platform ${platform_name} -sysproj ${sysproj_name}
+        
+        app config -name ${app_name} libraries "m"
+        
+        set app_sources [file join ${PmodNAV_basic_example::script_dir} sources]
+        set drivers [file join [file dirname [file dirname ${PmodNAV_basic_example::script_dir}]] sources]
+        importsources -name ${app_name} -path ${app_sources}
+        importsources -name ${app_name} -path ${drivers}
+        # note: configure the linker script?
+    }
+}
+
+PmodNAV_basic_example::init
